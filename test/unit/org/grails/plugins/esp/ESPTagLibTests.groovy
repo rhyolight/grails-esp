688597ae9632772d7fb5e8c221e9f4b196c5c270
Matthew Taylor <rhyolight@gmail.com>
2009-06-18 08:42:51 -0500

    Added one measly test for taglib for the most basic thing I could.

--- test/unit/org/grails/plugins/esp/ESPTagLibTests.groovy
+++ test/unit/org/grails/plugins/esp/ESPTagLibTests.groovy
@@ -3,6 +3,8 @@ package org.grails.plugins.esp
 import grails.test.*
 
 class ESPTagLibTests extends TagLibUnitTestCase {
+    
+
     protected void setUp() {
         super.setUp()
     }
@@ -11,7 +13,23 @@ class ESPTagLibTests extends TagLibUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testJsResourceOutputsProperTag() {
+        tagLib.metaClass.getControllerName = { -> println "FUCK"; "myController" }
+        tagLib.metaClass.getActionName = { -> "myAction" }
+        def gCalled = false
+        tagLib.metaClass.getG = { ->
+            [
+                createLink:{ params ->
+                    gCalled = true
+                    assertEquals 'resource', params.controller
+                    assertEquals 'getCached', params.action
+                    assertEquals 'js', params.params.type
+                    assertEquals 'myController.myAction', params.params.group
+                    "frank"
+                }
+            ]
+        }
+        tagLib.resource(type:'js')
+        assertEquals "<script src=\"frank\" type=\"application/javascript\"/>", tagLib.out.toString()
     }
 }
